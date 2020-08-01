311cb6627b8a6565f9d368421d0baea5ad1e6e67
Eric Niyonkuru <niyoeri6@gmail.com>
2020-08-01 18:23:03 +0200

    final

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 import productdb, {
-    bulkcreate
+    bulkcreate,getData,createEle
 } from './module.js'
 
 
 
-productdb("Productdb",{
+let db = productdb("Productdb",{
     products:`++id, name, seller, price`
 })
 
@@ -20,15 +20,132 @@ const btnread = document.getElementById('btn-read')
 const btnupdate = document.getElementById('btn-update')
 const btndelete = document.getElementById('btn-delete')
 
+
+//notfound
+const notfound = document.getElementById("notfound")
+
 //insert values using button
 
 btncreate.onclick = (event)=>{
     let flag = bulkcreate(db.products, {
-        name:productname.nodeValue,
-        seller:seller.nodeValue,
+        name:productname.value,
+        seller:seller.value,
         price:price.value
     })
-    console.log(flag)
+    // console.log(flag)
+    productname.value = seller.value = price.value = "";
+    getData(db.products, (data)=>{
+        // console.log(data)
+        userid.value = data.id +1 || 1
+    });
+    table();
+    let insertmsg = document.querySelector(".insertmsg");
+    getMsg(flag, insertmsg)
 }
 
+// update event
+btnupdate.onclick = ()=>{
+   const id =  parseInt(userid.value||0)
+   if(id){
+       db.products.update(id, {
+           name: productname.value,
+           seller: seller.value,
+           price: price.value
+       }).then((updated)=>{
+        //    let get = updated?`Data updated`:`Couldent updated`
+        let get = updated? true:false;
+        let updatemsg = document.querySelector(".updatemsg")
+        getMsg(get, updatemsg);
+       })
+   }
 
+}
+
+//delete records
+btndelete.onclick = ()=>{
+    db.delete();
+    db = productdb("Productdb",{
+        products:`++id, name, seller, price`
+    })
+    db.open()
+    table()
+    let deletemsg = document.querySelector(".deletmsg")
+    getMsg(true, deletemsg)
+}
+
+// window onload event
+window.onload = ()=>{
+    textID(userid)
+}
+
+function textID(textboxid){
+    getData(db.products, data=>{
+        textboxid.value = data.id + 1|| 1;
+
+    })
+}
+
+btnread.onclick = table;
+
+function table(){
+    const tbody = document.getElementById("tbody")
+
+    while(tbody.hasChildNodes()){
+        tbody.removeChild(tbody.firstChild);
+    }
+    getData(db.products, (data)=>{
+        if(data){
+            createEle("tr", tbody, tr=>{
+                for(const value in data){
+                    createEle("td",tr, td=>{
+                        td.textContent = data.price === data[value]?`$${data[value]}`:data[value]
+                    })
+                }
+                createEle("td", tr, td=>{
+                    createEle("i", td, i=>{
+                        i.className += "fas fa-edit btnedit"
+                        i.setAttribute(`data-id`, data.id)
+                        i.onclick = editbtn;
+                    })
+                })
+                createEle("td", tr, td=>{
+                    createEle("i", td, i=>{
+                        i.className += "fas fa-trash btndelete"
+                        i.setAttribute(`data-id`, data.id)
+                        i.onclick = deletebtn;
+                    })
+                })
+            })
+        }else{
+            notfound.textContent = "No record found in database"
+        }
+    })
+}
+
+function editbtn(event){
+    let id = parseInt(event.target.dataset.id)
+    db.products.get(id, data =>{
+        userid.value = data.id||0
+        productname.value = data.name||""
+        seller.value = data.seller||""
+        price.value = data.price||""
+   })
+}
+
+function deletebtn(event){
+    let id = parseInt(event.target.dataset.id);
+    db.products.delete(id)
+    table()
+}
+
+function getMsg(flag, element){
+    if(flag){
+        element.className+="movedown"
+
+        setTimeout(()=>{
+            element.classList.forEach(classname =>{
+                classname == "movedown"? undefined:element.classList.remove("movedown")
+            });
+        },4000);
+    }
+}
--- js/module.js
+++ js/module.js
@@ -3,11 +3,6 @@ const productdb = (dbname, table)=>{
     const db = new Dexie(dbname)
     db.version(1).stores(table)
     db.open()
-
-// const db = new Dexie('myDb');
-// db.version(1).stores({
-//     friends:`name,age`
-// })
  return db
 }
 //insert function
@@ -15,7 +10,7 @@ const productdb = (dbname, table)=>{
 const bulkcreate = (dbtable, data)=>{
     let flag = empty(data)
     if(flag){
-    dbtable.bulkAdd([data])
+        dbtable.bulkAdd([data])
         console.log("data inserted successfully")
     }else{
         console.log('Please provide data ....!')
@@ -27,16 +22,56 @@ const bulkcreate = (dbtable, data)=>{
 const empty = object =>{
     let flag = false
     for(const value in object){
-        if(object[value]!="" && object.hasOwnProperty(value)){
+        if(object[value] != "" && object.hasOwnProperty(value)){
             flag = true
         }else{
             flag = false
         }
     }
+    return flag
+}
+//get data from database
+const getData = (dbtable, fn)=>{
+    let index = 0;
+    let obj = {};
+
+    dbtable.count((count)=>{
+        if(count){
+            dbtable.each(table =>{
+                // console.log(table)
+                obj = Sortobj(table)
+                fn(obj, index++)
+            })
+        }else{
+            fn(0)
+        }
+    })
+}
+
+//create tags
+
+const createEle = (tagname, appendTo, fn)=>{
+    const element = document.createElement(tagname)
+    if(appendTo) appendTo.appendChild(element)
+    if(fn) fn(element)
+}
+
+// sort object
+const Sortobj = sortobj =>{
+    let obj = {};
+    obj = {
+        id: sortobj.id,
+        name: sortobj.name,
+        seller: sortobj.seller,
+        price: sortobj.price
+    }
+    return obj
 }
 
 
 export default productdb;
 export{
-    bulkcreate  
+    bulkcreate  ,
+    getData,
+    createEle
 }
